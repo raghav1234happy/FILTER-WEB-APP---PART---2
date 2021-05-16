7638bb43f7438b7c7234adb67d4aadbe363e7254
whitehat jr <71430109+raghav1234happy@users.noreply.github.com>
2021-05-16 09:08:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function preload() {
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+}
+
+function modelLoaded() {
+    console.log("Model has loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.mustache.x);
+        console.log("nose y = " + results[0].pose.mustache.y);
+    }
+}
+
